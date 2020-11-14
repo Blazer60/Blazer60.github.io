7bbc3a104785a307f0a6d4ff02b7f74c69b3bade
Ryan Purse <ryanpurse@gmail.com>
2020-11-14 15:25:09 +0000

    Setup root folder directories

---
+++ js/master.js
@@ -0,0 +1,8 @@
+/*
+ * Master JS script that is added to all pages. It's functionality is global.
+ * pages that have specific functions will have additional JavaScript files attached to them.
+ * Created On: 14/11/2020
+ * @author: Ryan Purse
+ */
+
+
