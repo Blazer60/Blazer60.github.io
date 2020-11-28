7d3e37f5d2fa7e20630e426aa20e4521aa272264
Ryan Purse <ryanpurse@gmail.com>
2020-11-28 15:25:16 +0000

    Created Dynamic gallery

---
+++ js/gallery.js
@@ -0,0 +1,55 @@
+/*
+ * gallery.js is used to load the images into
+ * the gallery and change them based on which
+ * one is clicked
+ * Created On: 27/11/2020
+ * @author: Ryan Purse
+ */
+
+// Globals
+const titleElement = document.getElementById("title");
+let imageElement = document.getElementById("image");
+const infoElement = document.getElementById("info");
+const sideBar = document.getElementById("side-bar");
+let images = [];
+
+const urls = [
+    "../img/gallery/aggressor.jpg",
+    "../img/gallery/Belakor.jpg",
+    "../img/gallery/Belakor.jpg",
+    "../img/gallery/belakorBack.jpg",
+    "../img/gallery/bloodletters.jpg",
+    "../img/gallery/corvusBlackstar.jpg",
+    "../img/gallery/eldarFalcon.jpg",
+    "../img/gallery/eldarFalconBack.jpg",
+    "../img/gallery/eldarFalconCockpit.jpg",
+    "../img/gallery/eldarFalconSideEngine.jpg",
+    "../img/gallery/eldarRanger.jpg",
+    "../img/gallery/eldarRangerBack.jpg",
+    "../img/gallery/eldarSquad.jpg",
+    "../img/gallery/fleshHounds.jpg",
+    "../img/gallery/khorneDeamonPrince.jpg",
+    "../img/gallery/khorneDeamonPrinceBack.jpg",
+    "../img/gallery/reiverSquad.jpg",
+    "../img/gallery/vostroyanGuardSquad.jpg",
+    "../img/gallery/vostroyanMedic.jpg",
+    "../img/gallery/vostroyanMortarTeam.jpg",
+]
+
+function createImage(src, id) {
+    let img = document.createElement("img");
+    img.src = src;
+    img.className = "side-bar-image";
+    img.addEventListener("click", function () {
+        imageElement.src = src;
+        titleElement.innerText = id;
+        infoElement.innerText = "hello.";
+    });
+    sideBar.appendChild(img);
+}
+
+window.onload = function () {
+    for (let i = 0; i < urls.length; i++) {
+        createImage(urls[i], i);
+    }
+}
