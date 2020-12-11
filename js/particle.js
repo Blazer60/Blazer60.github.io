09dd43ef56966d10bf82badce370b8391a1b05c2
Ryan Purse <ryanpurse@gmail.com>
2020-12-11 20:35:59 +0000

    Particles now rotate

--- js/particle.js
+++ js/particle.js
@@ -10,12 +10,12 @@ function get_random_number(min, max) {
 
 class Particle {
     constructor(pos) {
-        this.vel = [0, 0];
+        this.vel = [get_random_number(-40, 40), get_random_number(-40, 40)];
         this.acc = [get_random_number(-40, 40), get_random_number(-40, 40)];
-        this.size = 30.0;
+        this.size = get_random_number(30, 40);
         this.decay_rate = 10.0;
         this.rotation = 0;
-        this.rotation_speed = 0;
+        this.rotation_speed = get_random_number(-3.14 / 16, 3.14 / 16);
         this.alive = true;
         this.pos = [
             pos[0] - this.size / 2,
@@ -32,6 +32,9 @@ class Particle {
         this.pos[0] += this.vel[0] * delta_time;
         this.pos[1] += this.vel[1] * delta_time;
 
+        /* Update the rotation */
+        this.rotation += this.rotation_speed;
+
         /* Update size */
         this.size = Math.max(0, this.size - (this.decay_rate * delta_time));
 
@@ -42,7 +45,19 @@ class Particle {
     }
 
     render(ctx) {
+        /* Stops rotation being applied exponentially */
+        ctx.save();
+        ctx.beginPath();
+        /*
         ctx.fillStyle = "rgb(182, 251, 0)";
         ctx.fillRect(this.pos[0], this.pos[1], this.size, this.size);
+         */
+        ctx.translate(this.pos[0] + this.size / 2, this.pos[1] + this.size / 2);
+        ctx.rotate(this.rotation);
+        ctx.rect(-this.size / 2, -this.size / 2, this.size, this.size);
+        ctx.fillStyle = "rgb(182, 251, 0)";
+        ctx.fill();
+
+        ctx.restore();
     }
 }
